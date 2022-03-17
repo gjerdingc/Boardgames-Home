035016d33156b62acde314ecaeaf31c504e7aecb
Christian Gjerdingen <gjerdingc@gmail.com>
2022-03-17 09:44:05 +0100

    Allowed server IP in settings file

--- app/boardgames_home/settings.py
+++ app/boardgames_home/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.brettspill.net', 'boardgames', 'localhost', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['http://*.brettspill.net', 'http://*.127.0.0.1']
+ALLOWED_HOSTS = ['.brettspill.net', 'boardgames', 'localhost', '127.0.0.1', '10.0.0.15']
+CSRF_TRUSTED_ORIGINS = ['http://*.brettspill.net', 'http://*.127.0.0.1', '10.0.0.15']
 
 
 # Application definition
